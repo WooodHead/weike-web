812090dc28908d5f9b5fc135f02055b9938a142d
Wushaowei001 <jcwsw129@gmail.com>
2016-07-12 15:35:01 +0800

    init

---
+++ models/weike.js
@@ -0,0 +1,12 @@
+var mongoose=require('mongoose');
+var Schema=mongoose.Schema;
+
+var weikeSchema=new Schema(
+{
+	word:String,
+	pron:String,
+	definition:[String]
+	
+});
+
+module.exports=mongoose.model('Weike',weikeSchema);
